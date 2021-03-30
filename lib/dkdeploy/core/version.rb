0836e5600d89d7f3bc3999a44871a00596c2d0fb
Nicolai Reuschling <nicolai.reuschling@dkd.de>
2021-03-30 08:47:55 +0200

    prepare release v10.0.0

--- lib/dkdeploy/core/version.rb
+++ lib/dkdeploy/core/version.rb
@@ -5,9 +5,9 @@ module Dkdeploy
     # Class for version number
     #
     class Version
-      MAJOR = 9
-      MINOR = 3
-      PATCH = 1
+      MAJOR = 10
+      MINOR = 0
+      PATCH = 0
 
       def self.to_s
         [MAJOR, MINOR, PATCH].join('.')
